998702a0022057e8dbe00025c8e3ad922a679b69
Nguyen Duc Hieu <ndhieu1994@gmail.com>
2017-05-11 01:07:18 +0200

    done md5

--- Uebung5x6/src/de/eva/ZusatzaufgabeMD5/Md5Calculator.java
+++ Uebung5x6/src/de/eva/ZusatzaufgabeMD5/Md5Calculator.java
@@ -8,58 +8,64 @@ import java.security.NoSuchAlgorithmException;
  * Klasse zur Berechnung von MD5 Hashes von ganzen Zahlen.
  *
  * <p><b>Die Klasse ist nicht Thread Safe!!!</b></p>
- * 
+ *
  * @author Yves Annanias, Dan Haeberlein
  *
  */
 public class Md5Calculator {
 
-	/**
-	 * Das Objekt md5 vom Typ MessageDigest erlaubt die Ausfuehrung des MD5-Algorithmus
-	 *	http://download.oracle.com/javase/7/docs/api/java/security/MessageDigest.html
-	 */
-	private MessageDigest md5;
-	
-	public Md5Calculator() throws NoSuchAlgorithmException {
-		md5 =  MessageDigest.getInstance("MD5");
-	}
+  /**
+   * Das Objekt md5 vom Typ MessageDigest erlaubt die Ausfuehrung des MD5-Algorithmus
+   * http://download.oracle.com/javase/7/docs/api/java/security/MessageDigest.html
+   */
+  private MessageDigest md5;
 
-	/**
-	 * Methode, um den Hash einer ganzen Zahl zu berechnen.
-	 * 
-	 * @param hashable ganze Zahl
-	 * @return Hash-Wert der ganzen Zahl
-	 */
-	public Md5Hash getHash(int hashable){
-		// notwendig, um die MD5 Berechnung mehrmals durchfuehren zu koennen
-		md5.reset();
-		// Mit dieser Anweisung wird ein ByteBuffer gross genug fuer ein int-Wert angelegt,
-		// in den Buffer wird der Wert des Feldes "hashable" geschrieben
-		// und als array in die Variable message geschrieben
-		//
-		byte[] message = ByteBuffer.allocate(4).putInt(hashable).array();
-		// Message repraesentiert unsere Klartext-Nachricht, 
-		// welche im naechsten Schritt der MD5 Hash Berechnung unterzogen wird
-		md5.update(message);
-		// Umwandlung in die Md5Hash Klasse, um den Kollisionstest durchführen zu koennen 
-		return new Md5Hash(md5.digest());
-	}
-	
-	public static void main(String[] args) throws NoSuchAlgorithmException {
-		// vorgegebener Hash-Wert der ganzen Zahl 5 
-		Md5Hash md5HashOfIntValue5 = new Md5Hash(-4954387507941444427L, 7434082506426283588L);
-		// Instantiierung der Hashberechnungsklasse
-		Md5Calculator md5Calculator = new Md5Calculator();
-		// errechneter Hash-Wert der ganzen Zahl 5
-		Md5Hash calculatedHashValue = md5Calculator.getHash(5);
-		// Kollisionstest
-		boolean hasSameHash = calculatedHashValue.equals(md5HashOfIntValue5);
-		// Ausgabe Ergebnis
-		if(hasSameHash) {
-			System.out.println("encoded und hash sind gleich");
-		} else {
-			System.out.println("encoded und hash sind nicht gleich");
-		}
-	}
+  public Md5Calculator() throws NoSuchAlgorithmException {
+    md5 = MessageDigest.getInstance("MD5");
+  }
+
+  /**
+   * Methode, um den Hash einer ganzen Zahl zu berechnen.
+   *
+   * @param hashable ganze Zahl
+   * @return Hash-Wert der ganzen Zahl
+   */
+  public Md5Hash getHash(int hashable) {
+    // notwendig, um die MD5 Berechnung mehrmals durchfuehren zu koennen
+    md5.reset();
+    // Mit dieser Anweisung wird ein ByteBuffer gross genug fuer ein int-Wert angelegt,
+    // in den Buffer wird der Wert des Feldes "hashable" geschrieben
+    // und als array in die Variable message geschrieben
+    //
+    byte[] message = ByteBuffer.allocate(4).putInt(hashable).array();
+    // Message repraesentiert unsere Klartext-Nachricht,
+    // welche im naechsten Schritt der MD5 Hash Berechnung unterzogen wird
+    md5.update(message);
+    // Umwandlung in die Md5Hash Klasse, um den Kollisionstest durchführen zu koennen
+    return new Md5Hash(md5.digest());
+  }
+
+  public static void main(String[] args) throws NoSuchAlgorithmException {
+    // vorgegebener Hash-Wert der ganzen Zahl 5
+		//Md5Hash md5HashOfIntValue5 = new Md5Hash(-4954387507941444427L, 7434082506426283588L);
+    Md5Hash md5HashOfIntValue5 = new Md5Hash(5103841542109130789L, 33046678707426369L);
+    // Instantiierung der Hashberechnungsklasse
+    Md5Calculator md5Calculator = new Md5Calculator();
+    // errechneter Hash-Wert der ganzen Zahl 5
+		//Md5Hash calculatedHashValue = md5Calculator.getHash(5);
+
+    for (int i = 0; i <= 100; i++) {
+      Md5Hash calculatedHashValue = md5Calculator.getHash(i);
+      System.out.print(i + ": ");
+      // Kollisionstest
+      boolean hasSameHash = calculatedHashValue.equals(md5HashOfIntValue5);
+      // Ausgabe Ergebnis
+      if (hasSameHash) {
+        System.out.println("encoded und hash sind gleich");
+      } else {
+        System.out.println("encoded und hash sind nicht gleich");
+      }
+    }
+  }
 
 }
--- Uebung5x6/src/de/eva/ZusatzaufgabeMD5/Md5CollisionFinder.java
+++ Uebung5x6/src/de/eva/ZusatzaufgabeMD5/Md5CollisionFinder.java
@@ -2,7 +2,66 @@ package de.eva.ZusatzaufgabeMD5;
 
 import java.security.NoSuchAlgorithmException;
 
-public class Md5CollisionFinder {
+public class Md5CollisionFinder extends Thread {
 
-	
+  private int lowerBound, upperBound;
+  private Md5Hash hash;
+
+  public Md5CollisionFinder(int lowerBound, int upperBound, Md5Hash hash) {
+    this.lowerBound = lowerBound;
+    this.upperBound = upperBound;
+    this.hash = hash;
+  }
+
+  public int findCollision() throws NoSuchAlgorithmException{
+    Md5Calculator calc = new Md5Calculator();
+    for(int i = lowerBound; i <= upperBound; i++){
+      Md5Hash calculatedHash = calc.getHash(i);
+      boolean isCollision = hash.equals(calculatedHash);
+      if(isCollision){
+        return i;
+      }
+    }
+    String message = String.format("collision not found in interval [%d,%d]", lowerBound, upperBound);
+    throw new IllegalArgumentException(message);
+  }
+
+  @Override
+  public void run() {
+    try {
+      System.out.println("found collision: " + findCollision());
+    } catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) throws NoSuchAlgorithmException, InterruptedException {
+
+    int n = Integer.MAX_VALUE;
+    int numberOfThreads = 2;
+    Thread[] finderThreads = new Thread[numberOfThreads];
+
+
+    Md5Hash hash = new Md5Hash(8248337957008271949L, 7217552169022328908L);
+    //Md5CollisionFinder finder = new Md5CollisionFinder(0, Integer.MAX_VALUE, hash);
+    long start = System.currentTimeMillis();
+    for (int i = 0; i < numberOfThreads; i++) {
+      finderThreads[i] = new Md5CollisionFinder(
+        i * (n / numberOfThreads),
+        (i + 1) * (n / numberOfThreads),
+        hash);
+    }
+
+    for (Thread t : finderThreads) {
+      t.start();
+    }
+
+    for (Thread t : finderThreads) {
+      t.join();
+    }
+
+   long stop = System.currentTimeMillis();
+    long time = stop - start;
+    System.out.println("time taken: " + time + "ms");
+  }
 }
